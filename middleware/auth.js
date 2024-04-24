22ff0850d1daf8ce86de21571a606d5b7d1b2d1c
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-25 01:19:19 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,31 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 exports.isAuthenticated = async (req, res, next) => {
+  const { token } = req.body;
+  console.log(token, "the token is");
 
-    const { token } = req.body;
-    if (!token) {
-        return res.status(400).json({
-            message : "Please login first"
-        })
-    }
+  if (!token) {
+    return res.status(400).json({
+      message: "Please login first",
+    });
+  }
 
-    const {id }= await jwt.verify(token, process.env.JWT_SECRET_KEY);
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findOne({
-        _id : id
-    })
-    if (req.user != null) {
-        
-        next();
+      _id: id,
+    });
+    if (req.user !== null) {
+      next();
     } else {
-        return res.status(400).json({
-          message: "User not found login again",
-        });
+      return res.status(400).json({
+        message: "User not found. Please login again",
+      });
     }
-}
+  } catch (error) {
+    // Handle invalid token error
+    return res.status(401).json({
+      message: "Invalid token. Please login again",
+    });
+  }
+};
